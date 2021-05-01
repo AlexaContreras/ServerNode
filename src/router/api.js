fbf11553cf7142b1326b559d69d3b291aebd2905
AlexaContreras <root@192.168.1.9>
2021-05-01 09:27:39 -0300

    Apis funcionando

--- src/router/api.js
+++ src/router/api.js
@@ -1,48 +1,57 @@
-const { Router, response } = require('express');
+const { Router } = require('express');
 const axios = require('axios');
+
 const router = Router();
-const handleData = require('./helpers/handleData').handleData;
-const handleDataId = require('./helpers/handleData').handleDataId;
+const { handleData } = require('./helpers/handleData');
+const { handleDataId } = require('./helpers/handleData');
 
 router.get('/', (req, res) => {
-
   axios
-    .get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.query}&limit=4`)
-    .then(response => {
-
-      let data = response.data;
-      res.send(handleData(data))
-
-
-    }).catch(error => {
+    .get(
+      `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.query}&limit=4`,
+    )
+    .then((response) => {
+      const { data } = response;
+      res.send(handleData(data));
+    })
+    .catch((error) => {
       res.status(404).send(error);
     });
 });
 
 router.get('/:id', async (req, res) => {
-
   let itemToShow;
-
   try {
-    await axios.get(`https://api.mercadolibre.com/items/${req.params.id}`)
-      .then(response => {
-        itemToShow = handleDataId(response.data)
+    await axios
+      .get(`https://api.mercadolibre.com/items/${req.params.id}`)
+      .then((response) => {
+        itemToShow = handleDataId(response.data);
       });
   } catch (error) {
-    throw new Error(error.message)
+    throw new Error(error.message);
   }
 
   try {
-    await axios.get(`https://api.mercadolibre.com/items/${req.params.id}/description`)
-    .then(response => {
-      itemToShow.description = response.data.plain_text;
-    });
+    await axios
+      .get(`https://api.mercadolibre.com/items/${req.params.id}/description`)
+      .then((response) => {
+        itemToShow.item.description = response.data.plain_text;
+      });
   } catch (error) {
-    throw new Error(error.message)
+    throw new Error(error.message);
   }
-  
-  res.send(itemToShow)
 
+  try {
+    await axios
+      .get(`https://api.mercadolibre.com/categories/${itemToShow.item.category_id}`)
+      .then((response) => {
+        itemToShow.categories = response.data.path_from_root;
+      });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+  res.send(itemToShow);
 });
 
 module.exports = router;
--- src/router/helpers/handleData.js
+++ src/router/helpers/handleData.js
@@ -1,74 +1,81 @@
 const handleData = ({ results, available_filters, filters }) => {
+  const filterCategories = [];
 
-    let filterCategories = [];
-
-    if (filters.length != 0) {
-        filters[0].values[0].path_from_root.map((categorie) => {
-            filterCategories.push(categorie)
-        });
-    } else {
-        for (let index = 0; index < 4; index++) {
-            filterCategories.push(available_filters[0].values[index])
-
-        }
+  if (filters.length !== 0) {
+    filters[0].values[0].path_from_root.map((categorie) => filterCategories.push(categorie));
+  } else {
+    for (let index = 0; index < 4; index++) {
+      filterCategories.push(available_filters[0].values[index]);
     }
+  }
 
-    let author = {
-        name: "Alexa",
-        lastname: "Contreras"
+  const author = {
+    name: 'Alexa',
+    lastname: 'Contreras',
+  };
+
+  const categories = filterCategories;
+
+  const items = results.map((result) => {
+    const newResult = {
+      id: result.id,
+      title: result.title,
+      price: {
+        currency: result.currency_id,
+        amount: result.price,
+      },
+      picture: result.thumbnail,
+      condition: result.condition,
+      free_shipping: result.shipping.free_shipping,
     };
 
-    let categories = filterCategories;
+    return newResult;
+  });
 
-    let items = results.map(result => {
-        let newResult = {
-            id: result.id,
-            title: result.title,
-            price: {
-                currency: result.currency_id,
-                amount: result.price
-            },
-            picture: result.thumbnail,
-            condition: result.condition,
-            free_shipping: result.shipping.free_shipping
-        }
+  const products = {
+    author,
+    categories,
+    items,
+  };
 
-        return newResult;
-    })
+  return products;
+};
 
-    let products = {
-        author: author,
-        categories: categories,
-        items: items
-    }
+const handleDataId = ({
+  id,
+  title,
+  category_id,
+  price,
+  currency_id,
+  pictures,
+  condition,
+  shipping,
+  sold_quantity,
+}) => {
+  const item = {
+    author: {
+      name: 'Alexa',
+      lastname: 'Contreras',
+    },
+    categories: '',
+    item: {
+      id,
+      title,
+      price: {
+        currency: currency_id,
+        amount: price,
+        decimals: '0',
+      },
+      category_id,
+      picture: pictures[0].url,
+      condition,
+      free_shipping: shipping.free_shipping,
+      sold_quantity,
+      description: '',
+    },
+  };
 
-    return products;
-}
-
-
-let handleDataId = ({ id, title, category_id, price, currency_id, pictures, condition, shipping, sold_quantity }) => {
-    let item = {
-        author: {
-            name: 'Alexa',
-            lastname: 'Contreras',
-        },
-        item: {
-            id: id,
-            title: title,
-            price: {
-                currency: currency_id,
-                amount: price,
-                decimals: '0',
-            },
-            picture: pictures[0].url,
-            condition: condition,
-            free_shipping: shipping.free_shipping,
-            sold_quantity: sold_quantity,
-            description: ''
-        }
-    }
-
-    return item;
-}
+  return item;
+};
 
 module.exports = { handleData, handleDataId };
